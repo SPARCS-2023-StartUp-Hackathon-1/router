90c7c42bf95a8940831fd391032717bb41c9940d
ptptsw <ptptsw34@gmail.com>
2023-01-28 22:50:17 +0900

    simple map

--- front/src/screens/TravelScreen/index.js
+++ front/src/screens/TravelScreen/index.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-const TravelScreen = () => {
-  return <></>;
+const containerStyle = {
+  width: "400px",
+  height: "400px",
 };
 
-export default TravelScreen;
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+const positions = [
+  {
+    lat: 37.772,
+    lng: -122.214,
+  },
+  {
+    lat: 37.672,
+    lng: -122.219,
+  },
+  {
+    lat: 37.832,
+    lng: -122.424,
+  },
+];
+
+function MyComponent() {
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyB89A46XhoFozfegjbh7gnPzh9FiSQwRbo">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center} // set senter
+        zoom={10}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <Marker
+          icon={
+            "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+          }
+          position={positions[0]}
+        />
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default React.memo(MyComponent);
