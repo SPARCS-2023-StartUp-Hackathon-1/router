fc62d5ea1dd9ad770c1acead8dcfc43967257d07
ptptsw <ptptsw34@gmail.com>
2023-01-28 17:14:09 +0900

    add cluster api

--- api-embedding/api/main.py
+++ api-embedding/api/main.py
@@ -5,10 +5,20 @@ from datetime import datetime
 from image import getImageExifFromUrl
 from vit import getEmbeddingVectorFromUrl
 
+import pandas as pd
+
+
 app = FastAPI()
 
 class ExtractBody(BaseModel):
     imageUrl: str
+
+class clusteringBody(BaseModel):
+    time: datetime #??
+    latitude: float
+    longitude: float
+    vector: List # ??
+    valid: bool
 
 @app.post("/extract")
 def extractHandler(body: ExtractBody):
@@ -25,3 +35,33 @@ def extractHandler(body: ExtractBody):
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="error")
+
+@app.post("/clustering")
+def clusteringHandler(body: [clusteringBody]):
+    try:
+        sort_body = body.sorted(key=lambda x: x[0]) # sort by date
+        groupby_date =[]
+        subgroup = [sort_body[0]]
+        for i in range(1, len(image)):
+            if sort_body[i-1].datetime != sort_body[i].datetime:
+                groupby_date.append(subgroup)
+                subgroup=[]
+            else:
+                subgroup.append(sort_body[i-1])
+
+
+
+
+
+
+
+        # group by date
+        # in date group, group by lati,logitude
+        # return result
+
+
+
+            
+
+
+
--- api-router/src/route/image.js
+++ api-router/src/route/image.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const awsS3 = require("../modules/awsS3");
 const { imageModel } = require("../modules/mongo");
@@ -25,7 +26,7 @@ router.post("/upload/getPUrl", async (req, res) => {
     const key = `image-ori/${image._id}`;
     awsS3.getUploadPUrlPost(key, type, (err, data) => {
       if (err) {
-        console.error(e);
+        console.error(err);
         return res.status(500).send("internal server error");
       }
       data.fields["Content-Type"] = type;
@@ -60,11 +61,33 @@ router.post("/upload/complete", async (req, res) => {
 
     const key = `image-ori/${image._id}`;
     awsS3.foundObject(key, (err, data) => {
+      console.log(key);
+      console.log(err);
+      console.log(data);
       if (err) return res.json({ result: false });
       // ToDo : 이미지 정보 추출 됬는지
       // ToDo : 이미지 추출 정보 저장
+
+      // ToDo : test post cost
+      const options = {
+        uri: "http://router.hackathon.sparcs.org:8080/extract",
+        method: "POST",
+        body: {
+          imageUrl: key,
+        },
+        json: true,
+      };
+      request.post(options, function (body) {
+        console.log(body);
+        const { datetime, latitude, logitude, vector } = body;
+        image["time"] = datetime;
+        image["latitude"] = latitude;
+        image["logitude"] = logitude;
+        image["vector"] = vector;
+      });
       res.json({ result: true });
     });
+    await image.save();
   } catch (e) {
     console.log(e);
     res.status(500).send("internal server error");
