d3e15221fbf5863d4f414f90fffaf8ec32574b8b
Geon Kim <geon6757@kaist.ac.kr>
2023-01-29 04:46:55 +0900

    Update googlemap.py

--- api-embedding/api/googlemap.py
+++ api-embedding/api/googlemap.py
@@ -5,15 +5,17 @@ import os
 load_dotenv()
 gmaps = googlemaps.Client(key=os.getenv('GOOGLEMAP_API_KEY'))
 
-def aroundSearch(lat, lng):
+def aroundSearch(lat, lng, ver="1"):
     try:
-        # 거리 이름 버전
-        # reverse_geocode_result = gmaps.reverse_geocode((lat, lng), language="ko")
-        # return reverse_geocode_result[0]['formatted_address']
-        # 가까운 장소 버전
-        result = gmaps.places_nearby(location=(lat, lng), radius=1000, language="ko")
-        names = [result['name'] for result in result['results']]
-        return names[0]
+        if ver == "1":
+            # 거리 이름 버전
+            reverse_geocode_result = gmaps.reverse_geocode((lat, lng), language="ko")
+            return reverse_geocode_result[0]['formatted_address']
+        else:
+            # 가까운 장소 버전
+            result = gmaps.places_nearby(location=(lat, lng), radius=1000, language="ko")
+            names = [result['name'] for result in result['results']]
+            return names[0]
     except:
         return ""
 
