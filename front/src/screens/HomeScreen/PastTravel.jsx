0a245b250bf08baf61bdbf575a126863baa88220
imYourChoi <jeehun@kaist.ac.kr>
2023-01-29 08:55:01 +0900

    Randomize recent travels

--- front/src/screens/HomeScreen/PastTravel.jsx
+++ front/src/screens/HomeScreen/PastTravel.jsx
@@ -43,29 +43,32 @@ const PastTravel = () => {
         }}
       >
         {trips.length ? (
-          trips.slice(0, 3).map((trip) => {
-            const date = trip.startTime.split("T")[0].split("-");
-            return (
-              <PhotoBox
-                key={trip._id}
-                path={trip.mainImage}
-                href={`/travel/${trip._id}`}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-end",
-                  }}
+          trips
+            .sort(() => 0.5 - Math.random())
+            .slice(0, 3)
+            .map((trip) => {
+              const date = trip.startTime.split("T")[0].split("-");
+              return (
+                <PhotoBox
+                  key={trip._id}
+                  path={trip.mainImage}
+                  href={`/travel/${trip._id}`}
                 >
-                  <div style={{ marginBottom: 4 }}>
-                    {date[0].slice(2, 4)}년 {date[1]}월
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-end",
+                    }}
+                  >
+                    <div style={{ marginBottom: 4 }}>
+                      {date[0].slice(2, 4)}년 {date[1]}월
+                    </div>
+                    <div className="font-subtitle-small">{trip.name}</div>
                   </div>
-                  <div className="font-subtitle-small">{trip.name}</div>
-                </div>
-              </PhotoBox>
-            );
-          })
+                </PhotoBox>
+              );
+            })
         ) : (
           <GrayBox padding="24px 0">
             <div style={{ textAlign: "center" }}>여행 기록이 없어요</div>
