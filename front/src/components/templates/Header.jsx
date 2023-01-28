ab34ad4a8e1a33170ddf4dbcb01fcd36013a9c3c
Yumin Cho <dbals081216@gmail.com>
2023-01-29 02:34:49 +0900

    fix header

--- front/src/components/templates/Header.jsx
+++ front/src/components/templates/Header.jsx
@@ -4,14 +4,25 @@ import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRound
 
 const Header = () => {
   const navigate = useNavigate();
-  const styleContainer = {
-    width: "100%",
-    height: 32,
-    paddingTop: "calc(env(safe-area-inset-top))",
-    display: "flex",
-    alignItems: "center",
-  };
   const path = useLocation().pathname;
+  const styleContainer = {
+    ...{
+      width: "100%",
+      height: 32,
+      paddingTop: "calc(env(safe-area-inset-top))",
+      display: "flex",
+      alignItems: "center",
+      position: "fixed",
+      zIndex: "10",
+    },
+    ...(path.startsWith("/create") || path.startsWith("/travel")
+      ? {}
+      : {
+          background: "rgba(255, 255, 255, 0.75)",
+          webkitBackdropFilter: "blur(20px)",
+          BackdropFilter: "blur(20px)",
+        }),
+  };
   return (
     <div style={styleContainer}>
       {path.startsWith("/create") || path.startsWith("/travel") ? (
