7cafb1bf8f06dd25a7e2eaaa16d5ad4abd53c1a2
14Kgun <geon6757@kaist.ac.kr>
2023-01-28 20:47:10 +0900

    Fix main.py

--- api-embedding/api/main.py
+++ api-embedding/api/main.py
@@ -13,10 +13,14 @@ app = FastAPI()
 class ExtractBody(BaseModel):
     imageUrl: str
 
-class clusteringBody(BaseModel):
+class ImageInfo(BaseModel):
     time: datetime
     latitude: float
     longitude: float
+    vector: List[float]
+
+class ClusteringBody(BaseModel):
+    images: List[ImageInfo]
 
 @app.post("/extract")
 def extractHandler(body: ExtractBody):
@@ -35,16 +39,16 @@ def extractHandler(body: ExtractBody):
         raise HTTPException(status_code=400, detail="error")
 
 @app.post("/clustering")
-def clusteringHandler(body: [clusteringBody]):
+def clusteringHandler(body: ClusteringBody):
     try:
         images = body.images
-        sort_body = images.sorted(key=lambda x: x[0]) # sort by date
-        startTime = sort_body[0].time.date
-        endTime = sort_body[-1].time.date
+        sort_body = images.sorted(key=lambda x: x["time"]) # sort by date
+        startTime = sort_body[0]["time"].date
+        endTime = sort_body[-1]["time"].date
         
         cluster =[]
         subgroup = [sort_body[0]]
-        for i in range(1, len(image)):
+        for i in range(1, len(images)):
             if 3600 < diff(sort_body[i-1].datetime, sort_body[i].datetime) and 1 < dist(sort_body[i-1].latitude, sort_body[i-1].longitude, sort_body[i].latitude, sort_body[i].longitude):
                 cluster.append(subgroup)
                 subgroup=[]
@@ -58,8 +62,3 @@ def clusteringHandler(body: [clusteringBody]):
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="error")
-
-
-
-
-        
