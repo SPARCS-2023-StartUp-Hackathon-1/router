9d40b1303c6ecf111ad0a0fe19b93b0d861370ed
14Kgun <geon6757@kaist.ac.kr>
2023-01-28 16:41:58 +0900

    Remove dotenv

--- front/src/loadenv.js
+++ front/src/loadenv.js
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const env = { ...process.env, ...window["env"] };
 
 const nodeEnv = env.NODE_ENV;
--- front/src/screens/LoginScreen/index.jsx
+++ front/src/screens/LoginScreen/index.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/common/Button";
 import Input from "./Input";
 import Circle from "components/common/Circle";
 
+import axios from "utils/axios";
+
 const LoginScreen = () => {
+  const [loginId, setLoginId] = useState("");
+  const [loginPw, setLoginPw] = useState("");
+
+  const onClickLogin = () => {
+    if (loginId === "" || loginPw === "") {
+      return alert("아이디와 비밀번호를 입력해주세요.");
+    }
+  };
   const styleContainer = {
     position: "absolute",
     top: "50%",
@@ -26,7 +36,12 @@ const LoginScreen = () => {
           <div className="font-logo-large">ROUTER</div>
           <div className="font-text-large">routes taken, memories retraced</div>
         </div>
-        <Input />
+        <Input
+          loginId={loginId}
+          loginPw={loginPw}
+          onChangeLoginId={setLoginId}
+          onChangeLoginPw={setLoginPw}
+        />
         <Button
           type="primary"
           className="font-subtitle-small"
@@ -34,6 +49,7 @@ const LoginScreen = () => {
           padding="16px 0 20px"
           radius={30}
           style={{ marginBottom: 12 }}
+          onClick={onClickLogin}
         >
           로그인
         </Button>
