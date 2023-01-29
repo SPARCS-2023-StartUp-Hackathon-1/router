4b1993651180fc3498205e2ceeb3326a3b6bbef4
ptptsw <ptptsw34@gmail.com>
2023-01-29 10:49:28 +0900

    fix currentTrip

--- front/src/screens/HomeScreen/CurrentTravel.jsx
+++ front/src/screens/HomeScreen/CurrentTravel.jsx
@@ -8,8 +8,9 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 const CurrentTravel = () => {
-  const [onetrip, setTrip] = useState([]);
+  const [trips, setTrips] = useState([]);
   const loginInfo = useRecoilValue(loginInfoAtom);
+  const [date, setDate] = useState("");
   useEffect(() => {
     const f = async () => {
       const response = await axios.get(`/user/triplist/${loginInfo.id}`);
@@ -20,27 +21,45 @@ const CurrentTravel = () => {
             currTrip.push(trip);
           }
         }
-        setTrip(currTrip[0]);
+        console.log(currTrip[0].name);
+        console.log(currTrip[0].startTime);
+        setTrips(currTrip);
       }
     };
     if (loginInfo?.id) f();
   }, [loginInfo]);
   return (
-    <PhotoBox
-      height={228}
-      key={onetrip._id}
-      path={onetrip.mainImage}
-      href={`/travel/${onetrip._id}`}
-    >
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <ProfileImg width={32} />
-        <ProfileImg width={32} margin="0 0 0 -8px" />
-        <ProfileImg width={32} margin="0 0 0 -8px" />
-        <div className="font-text-small" style={{ marginLeft: 12 }}>
-          지금까지 <u>8</u>개의 핀이 기록되었어요
-        </div>
-      </div>
-    </PhotoBox>
+    <>
+      {trips.slice(0, 1).map((trip) => {
+        const date = trip.startTime.split("T")[0].split("-");
+        return (
+          <PhotoBox
+            height={228}
+            key={trip._id}
+            path={trip.mainImage}
+            href={`/travel/${trip._id}`}
+          >
+            <div className="font-text-large">
+              {date[0].slice(2, 4)}년 {date[1]}월
+            </div>
+            <div
+              className="font-subtitle-large"
+              style={{ margin: "4px 0 8px" }}
+            >
+              {trip.name}
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <ProfileImg width={32} />
+              <ProfileImg width={32} margin="0 0 0 -8px" />
+              <ProfileImg width={32} margin="0 0 0 -8px" />
+              <div className="font-text-small" style={{ marginLeft: 12 }}>
+                지금까지 <u>8</u>개의 핀이 기록되었어요
+              </div>
+            </div>
+          </PhotoBox>
+        );
+      })}
+    </>
   );
 };
 
